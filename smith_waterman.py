a64c0494f6752980f022c6cd288150ff5546c428
James Tan <james_tan97@outlook.com>
2025-08-10 01:59:41 -0400

    implement affine gaps

--- smith_waterman.py
+++ smith_waterman.py
@@ -4,45 +4,48 @@ import pathlib
 
 from sortedcontainers import SortedList
 
-
-# def build_matrix(a, b, match_score=3, gap_cost=2):
-#     num_rows, num_cols = len(a) + 1, len(b) + 1
-#     H = [[0] * num_cols for _ in range(num_rows)]
-#
-#     # TODO: don't have to allocate the entire matrix since I only need
-#     # the previous row to calculate the current row
-#     for i in range(1, num_rows):
-#         for j in range(1, num_cols):
-#             match = H[i - 1][j - 1] + (
-#                 match_score if a[i - 1] == b[j - 1] else -match_score
-#             )
-#             delete = H[i - 1][j] - gap_cost
-#             insert = H[i][j - 1] - gap_cost
-#             H[i][j] = max(match, delete, insert, 0)
-#     return H
-#
-# def fuzzy_match_matrix(query, candidate):
-#     matrix = build_matrix(candidate.lower(), query.lower())
-#     return max(e for l in matrix for e in l)
+MATCH_SCORE = 12
+MISMATCH_PENALTY = 6
+GAP_OPEN_PENALTY = 5
+GAP_EXTEND_PENALTY = 1
 
 
-# TODO: add affine gap
-def build_scores(a, b, match_score=3, gap_cost=2):
-    num_rows, num_cols = len(a) + 1, len(b) + 1
+def build_scores(needle, haystack):
+    num_rows, num_cols = len(needle) + 1, len(haystack) + 1
     prev = [0] * num_cols
     curr = [0] * num_cols
     max_val = 0
 
     for i in range(1, num_rows):
-        for j in range(1, num_cols):
-            match = prev[j - 1] + (
-                match_score if a[i - 1] == b[j - 1] else -match_score
-            )
-            delete = prev[j] - gap_cost
-            insert = curr[j - 1] - gap_cost
-            curr[j] = max(match, delete, insert, 0)
+        needle_char = needle[i - 1]
+        insert_gap_mask = True
+        delete_gap_mask = True
 
-        max_val = max(max(curr), max_val)
+        for j in range(1, num_cols):
+            haystack_char = haystack[j - 1]
+            match_score = prev[j - 1] + (
+                MATCH_SCORE if needle_char == haystack_char else -MISMATCH_PENALTY
+            )
+            # left
+            delete_gap_penalty = (
+                GAP_OPEN_PENALTY if delete_gap_mask else GAP_EXTEND_PENALTY
+            )
+            delete = prev[j] - delete_gap_penalty
+            # up
+            insert_gap_penalty = (
+                GAP_OPEN_PENALTY if insert_gap_mask else GAP_EXTEND_PENALTY
+            )
+            insert = curr[j - 1] - insert_gap_penalty
+
+            max_score = max(match_score, delete, insert)
+
+            match_mask = max_score == match_score
+            insert_gap_mask = (max_score != insert) or match_mask
+            delete_gap_mask = (max_score != delete) or match_mask
+
+            curr[j] = max_score
+            max_val = max(curr[j], max_val)
+
         prev = curr
         curr = [0] * num_cols
 
